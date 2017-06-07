74d6b1be169dcc4928733fe680fd77f57c02ed78
James Rippon <jrippon27@gmail.com>
2017-06-06 23:53:59 -0700

    Adding all project files
    I have the entire project files.

---
+++ money_calculator/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace money_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textOneHundred = new System.Windows.Forms.TextBox();
+            this.textFifty = new System.Windows.Forms.TextBox();
+            this.textTwenty = new System.Windows.Forms.TextBox();
+            this.textTen = new System.Windows.Forms.TextBox();
+            this.textFive = new System.Windows.Forms.TextBox();
+            this.txtOne = new System.Windows.Forms.TextBox();
+            this.textTwentyFiveCents = new System.Windows.Forms.TextBox();
+            this.textTenCents = new System.Windows.Forms.TextBox();
+            this.textFiveCents = new System.Windows.Forms.TextBox();
+            this.textOneCent = new System.Windows.Forms.TextBox();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textOneHundred
+            // 
+            this.textOneHundred.Location = new System.Drawing.Point(95, 58);
+            this.textOneHundred.Name = "textOneHundred";
+            this.textOneHundred.Size = new System.Drawing.Size(100, 20);
+            this.textOneHundred.TabIndex = 0;
+            this.textOneHundred.TextChanged += new System.EventHandler(this.textOneHundred_TextChanged);
+            // 
+            // textFifty
+            // 
+            this.textFifty.Location = new System.Drawing.Point(95, 103);
+            this.textFifty.Name = "textFifty";
+            this.textFifty.Size = new System.Drawing.Size(100, 20);
+            this.textFifty.TabIndex = 1;
+            this.textFifty.TextChanged += new System.EventHandler(this.textFifty_TextChanged);
+            // 
+            // textTwenty
+            // 
+            this.textTwenty.Location = new System.Drawing.Point(95, 151);
+            this.textTwenty.Name = "textTwenty";
+            this.textTwenty.Size = new System.Drawing.Size(100, 20);
+            this.textTwenty.TabIndex = 2;
+            this.textTwenty.TextChanged += new System.EventHandler(this.textTwentyFive_TextChanged);
+            // 
+            // textTen
+            // 
+            this.textTen.Location = new System.Drawing.Point(95, 194);
+            this.textTen.Name = "textTen";
+            this.textTen.Size = new System.Drawing.Size(100, 20);
+            this.textTen.TabIndex = 3;
+            this.textTen.TextChanged += new System.EventHandler(this.textTen_TextChanged);
+            // 
+            // textFive
+            // 
+            this.textFive.Location = new System.Drawing.Point(95, 239);
+            this.textFive.Name = "textFive";
+            this.textFive.Size = new System.Drawing.Size(100, 20);
+            this.textFive.TabIndex = 4;
+            this.textFive.TextChanged += new System.EventHandler(this.textFive_TextChanged);
+            // 
+            // txtOne
+            // 
+            this.txtOne.Location = new System.Drawing.Point(95, 282);
+            this.txtOne.Name = "txtOne";
+            this.txtOne.Size = new System.Drawing.Size(100, 20);
+            this.txtOne.TabIndex = 5;
+            this.txtOne.TextChanged += new System.EventHandler(this.txtOne_TextChanged);
+            // 
+            // textTwentyFiveCents
+            // 
+            this.textTwentyFiveCents.Location = new System.Drawing.Point(328, 58);
+            this.textTwentyFiveCents.Name = "textTwentyFiveCents";
+            this.textTwentyFiveCents.Size = new System.Drawing.Size(100, 20);
+            this.textTwentyFiveCents.TabIndex = 6;
+            this.textTwentyFiveCents.TextChanged += new System.EventHandler(this.textTwentyFiveCents_TextChanged);
+            // 
+            // textTenCents
+            // 
+            this.textTenCents.Location = new System.Drawing.Point(328, 103);
+            this.textTenCents.Name = "textTenCents";
+            this.textTenCents.Size = new System.Drawing.Size(100, 20);
+            this.textTenCents.TabIndex = 7;
+            this.textTenCents.TextChanged += new System.EventHandler(this.textTenCents_TextChanged);
+            // 
+            // textFiveCents
+            // 
+            this.textFiveCents.Location = new System.Drawing.Point(328, 151);
+            this.textFiveCents.Name = "textFiveCents";
+            this.textFiveCents.Size = new System.Drawing.Size(100, 20);
+            this.textFiveCents.TabIndex = 8;
+            this.textFiveCents.TextChanged += new System.EventHandler(this.textFiveCents_TextChanged);
+            // 
+            // textOneCent
+            // 
+            this.textOneCent.Location = new System.Drawing.Point(328, 194);
+            this.textOneCent.Name = "textOneCent";
+            this.textOneCent.Size = new System.Drawing.Size(100, 20);
+            this.textOneCent.TabIndex = 9;
+            this.textOneCent.TextChanged += new System.EventHandler(this.textOneCent_TextChanged);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(244, 320);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 10;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "$100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "$50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "$20";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "$10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "$5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(25, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "$1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(241, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 16);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "$ .25";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(241, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 16);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "$ .10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(241, 154);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 16);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "$ .05";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(241, 197);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 16);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "$ .01";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 390);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.textOneCent);
+            this.Controls.Add(this.textFiveCents);
+            this.Controls.Add(this.textTenCents);
+            this.Controls.Add(this.textTwentyFiveCents);
+            this.Controls.Add(this.txtOne);
+            this.Controls.Add(this.textFive);
+            this.Controls.Add(this.textTen);
+            this.Controls.Add(this.textTwenty);
+            this.Controls.Add(this.textFifty);
+            this.Controls.Add(this.textOneHundred);
+            this.Name = "Form1";
+            this.Text = "Money Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textOneHundred;
+        private System.Windows.Forms.TextBox textFifty;
+        private System.Windows.Forms.TextBox textTwenty;
+        private System.Windows.Forms.TextBox textTen;
+        private System.Windows.Forms.TextBox textFive;
+        private System.Windows.Forms.TextBox txtOne;
+        private System.Windows.Forms.TextBox textTwentyFiveCents;
+        private System.Windows.Forms.TextBox textTenCents;
+        private System.Windows.Forms.TextBox textFiveCents;
+        private System.Windows.Forms.TextBox textOneCent;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ money_calculator/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace money_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            //whole dollars
+            double oneHundred = 100;
+            double fifty = 50;
+            double twenty = 20;
+            double ten = 10;
+            double five = 5;
+            double one = 1;
+
+            //cents
+            double twentyFiveCents = 0.25;
+            double tenCents = 0.10;
+            double fiveCents = 0.05;
+            double oneCent = 0.01;
+
+            //answer
+            double answer;
+            //converts to double
+            oneHundred = double.Parse(textOneHundred.Text) * 100;
+            fifty = double.Parse(textFifty.Text) * 50;
+            twenty = double.Parse(textTwenty.Text) * 20;
+            ten = double.Parse(textTen.Text) * 10;
+            five = double.Parse(textFive.Text) * 5;
+            one = double.Parse(textOneCent.Text) * 1;
+
+            twentyFiveCents = double.Parse(textTwentyFiveCents.Text) * 0.25;
+            tenCents = double.Parse(textTenCents.Text) * 0.10;
+            fiveCents = double.Parse(textFiveCents.Text) * 0.05;
+            oneCent = double.Parse(textOneCent.Text) * 0.01;
+
+            answer = oneHundred + fifty + twenty + ten + five +
+                one + twentyFiveCents + tenCents + fiveCents + oneCent;
+            
+            MessageBox.Show(answer.ToString());
+
+        }
+
+        private void textOneHundred_TextChanged(object sender, EventArgs e)
+        {
+            double oneHundred;
+            oneHundred = double.Parse(textOneHundred.Text);
+        }
+
+        private void textFifty_TextChanged(object sender, EventArgs e)
+        {
+            double fifty;
+            fifty = double.Parse(textFifty.Text);
+        }
+
+        private void textTwentyFive_TextChanged(object sender, EventArgs e)
+        {
+            double twenty;
+            twenty = double.Parse(textTwenty.Text);
+        }
+
+        private void textTen_TextChanged(object sender, EventArgs e)
+        {
+            double ten;
+            ten = double.Parse(textTen.Text);
+        }
+
+        private void textFive_TextChanged(object sender, EventArgs e)
+        {
+            double five;
+            five = double.Parse(textFive.Text);
+        }
+
+        private void txtOne_TextChanged(object sender, EventArgs e)
+        {
+            double one;
+            one = double.Parse(textOneHundred.Text);
+        }
+
+        private void textTwentyFiveCents_TextChanged(object sender, EventArgs e)
+        {
+            double twentyFiveCents;
+            twentyFiveCents = double.Parse(textTwenty.Text);
+            
+        }
+
+        private void textTenCents_TextChanged(object sender, EventArgs e)
+        {
+            double tenCents;
+            tenCents = double.Parse(textTenCents.Text);
+
+        }
+
+        private void textFiveCents_TextChanged(object sender, EventArgs e)
+        {
+            double fiveCents;
+            fiveCents = double.Parse(textFiveCents.Text);
+        }
+
+        private void textOneCent_TextChanged(object sender, EventArgs e)
+        {
+            double oneCent;
+            oneCent = double.Parse(textOneCent.Text);
+        }
+    }
+}
